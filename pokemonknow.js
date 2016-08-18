d38787b738cbbca6357c4108cb04493ab86c6e38
Sharvari Desai <sharvaridesai@SDESAI-MBP-DAL.local.tld>
2016-08-18 14:33:26 -0500

    added a showImage function to call when image needs to appear

--- pokemonknow.js
+++ pokemonknow.js
@@ -4,25 +4,13 @@ pokeApp.controller('GuessController', ['$scope', function($scope) {
     $scope.guess = "";
 }]);
 
-/*
 $(document).ready(function(){
    for(var i = 0; i < 151; i++)
-     $("#boxes").append("<img src=\"transparent.png\" class=\"pimg\" width=\"30\" height=\"30\" />");  
+     $("#boxes").append("<img src=\"transparent.png\" class=\"pimg\" width=\"50\" height=\"50\" />");  
 });
-*/
 
-/*
-images = {
-    img1: "http://vignette3.wikia.nocookie.net/pokemon/images/c/c6/027Sandshrew_AG_anime.png/revision/latest?cb=20140906222809",
-    img2: "http://cdn.bulbagarden.net/upload/thumb/3/3d/058Growlithe.png/250px-058Growlithe.png",
-    img3: "http://www.freeiconspng.com/uploads/pokemon-png-24.png"
-};
-*/
 
-var element, filling = "";
-for (element in images) {
-    filling += "<img src=\"{0}\" class=\"pimg\" width=\"50\" height=\"50\" />"
-        .replace("{0}", images[element]);
+function showImage(pokeNum) {
+    var str = ":nth-child(" + pokeNum + ")";
+    $("#boxes > " + str).attr('src', "sprites/" + pokeNum + ".png");
 }
-
-document.getElementById("boxes").innerHTML = filling;
