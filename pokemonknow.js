0d6a157e844b11763c6815beeb82e6174ebf4f03
Sharvari Desai <sharvaridesai@SDESAI-MBP-DAL.local.tld>
2016-08-18 13:41:47 -0500

    Added empty boxes for the images

--- pokemonknow.js
+++ pokemonknow.js
@@ -4,10 +4,16 @@ pokeApp.controller('GuessController', ['$scope', function($scope) {
     $scope.guess = "";
 }]);
 
+$(document).ready(function(){
+   for(var i = 0; i < 151; i++)
+     $("#boxes").append("<img src=\"transparent.png\" class=\"pimg\" width=\"30\" height=\"30\" />");  
+});
+
+/*
 images = {
     img1: "http://vignette3.wikia.nocookie.net/pokemon/images/c/c6/027Sandshrew_AG_anime.png/revision/latest?cb=20140906222809",
     img2: "http://cdn.bulbagarden.net/upload/thumb/3/3d/058Growlithe.png/250px-058Growlithe.png",
-    img3: "http://vignette1.wikia.nocookie.net/pokemon/images/f/fc/025Pikachu_OS_anime_5.png/revision/20150101093704"
+    img3: "http://www.freeiconspng.com/uploads/pokemon-png-24.png"
 };
 
 var element, filling = "";
@@ -17,3 +23,4 @@ for (element in images) {
 }
 
 document.getElementById("boxes").innerHTML = filling;
+*/
