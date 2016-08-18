ff11087254f8261a3a0ddb05f4141af7fceefa65
Jack Magiera <jackmagiera@JMAGIERA-MBP-CHI.local>
2016-08-18 13:46:35 -0500

    moved stuff out of 'test'

--- test/pokeknow.js
+++ test/pokeknow.js
@@ -1,5 +1,5 @@
 app.factory("pokeknow", ["$http", function($http) {
-  return $http.get("http://pokeapi.co/api/v2/pokedex/kanto/")
+  return $http.get("http://pokeapi.kevgriffin.com/api/v2/pokedex/kanto/")
     .success(function(data) {
       return data;
     })
