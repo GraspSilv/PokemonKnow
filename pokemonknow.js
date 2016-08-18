8fd357d83d59131a9f615782e0f1138b0c4f5053
Sharvari Desai <desai.sharvari@gmail.com>
2016-08-18 15:20:42 -0500

    added countdown timer

--- pokemonknow.js
+++ pokemonknow.js
@@ -29,3 +29,43 @@ function showImage(pokeNum) {
     var str = ":nth-child(" + pokeNum + ")";
     $("#boxes > " + str).attr('src', "sprites/" + pokeNum + ".png");
 }
+
+// Countdown Timer
+
+var timeInMinutes = 2;
+var currentTime = Date.parse(new Date());
+var deadline = new Date(currentTime + timeInMinutes*60*1000);
+
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime){
+  var clock = document.getElementById(id);
+var minutesSpan = clock.querySelector('.minutes');
+var secondsSpan = clock.querySelector('.seconds');
+  function updateClock(){
+      var t = getTimeRemaining(endtime);
+        minutesSpan.innerHTML = t.minutes;
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+      if(t.total<=0){
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock(); // run function once at first to avoid delay
+    var timeinterval = setInterval(updateClock,1000);
+}
+
+initializeClock('clockdiv', deadline);
