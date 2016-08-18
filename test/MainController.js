dc91d9faa310528ab2577a6040e171272f92a0a5
Jack Magiera <jackmagiera@JMAGIERA-MBP-CHI.local>
2016-08-18 11:53:04 -0500

    filtered to 151

--- test/MainController.js
+++ test/MainController.js
@@ -2,7 +2,9 @@
 
 app.controller('MainController', ['$scope', 'pokeknow', function($scope, pokeknow) {
   pokeknow.success(function(data) {
-    $scope.pokemon = data;
+    $scope.species = data;
+    console.log(data.pokemon_entries);
+    $scope.quantity = 151;
   });
 
 }]);
