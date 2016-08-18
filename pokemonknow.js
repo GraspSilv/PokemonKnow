502e2a47ca2a003e89d676ba95079a672c183a3b
Jack Magiera <jackmagiera@JMAGIERA-MBP-CHI.local>
2016-08-18 13:51:10 -0500

    fixed conflicts

--- pokemonknow.js
+++ pokemonknow.js
@@ -1,10 +1,19 @@
-app.factory("pokemonknow", ["$http", function($http) {
-  return $http.get("http://pokeapi.kevgriffin.com/api/v2/pokedex/kanto/")
-    .success(function(data) {
-      return data;
-    })
+var pokeApp = angular.module('pokeApp', []);
 
-    .error(function(err) {
-      return err;
-    });
+pokeApp.controller('GuessController', ['$scope', function($scope) {
+    $scope.guess = "";
 }]);
+
+images = {
+    img1: "http://vignette3.wikia.nocookie.net/pokemon/images/c/c6/027Sandshrew_AG_anime.png/revision/latest?cb=20140906222809",
+    img2: "http://cdn.bulbagarden.net/upload/thumb/3/3d/058Growlithe.png/250px-058Growlithe.png",
+    img3: "http://vignette1.wikia.nocookie.net/pokemon/images/f/fc/025Pikachu_OS_anime_5.png/revision/20150101093704"
+};
+
+var element, filling = "";
+for (element in images) {
+    filling += "<img src=\"{0}\" class=\"pimg\" width=\"50\" height=\"50\" />"
+        .replace("{0}", images[element]);
+}
+
+document.getElementById("boxes").innerHTML = filling;
