4ca58ddecee9d374aa4ad1e14e5af974f7a791d3
Jack Magiera <jackmagiera@JMAGIERA-MBP-CHI.local>
2016-08-19 10:00:59 -0500

    fixed merge conflicts

--- pokemon.js
+++ pokemon.js
@@ -4,9 +4,5 @@ var Pokemon = function(pokemon_json) {
 	this.visible = false;
 	this.id_no = pokemon_json.entry_number;
 	this.poke_name = pokemon_json.pokemon_species.name;
-<<<<<<< HEAD
-	this.image = "img/" + this.id_no + ".png"
-=======
-	this.image = "img/" + this.id_no + ".png";
->>>>>>> 8c9f68d0c7f10f91c6a4fc5c429523ffc2728248
+	this.image = "sprites/" + this.id_no + ".png";
 };
